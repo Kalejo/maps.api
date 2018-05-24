ad0660911319716d58dfc91709e2174cb0951baa
Alexandro <alejo_alvarez67@hotmail.com>
2018-05-24 15:56:41 +0200

    marker addlistener

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
  	@lat = params['latitude'].to_f
  	@lng = params['longitude'].to_f
- 	p @lat;
- 	p @lng;
+ 	@msg = params['content']
+
   end
 end
